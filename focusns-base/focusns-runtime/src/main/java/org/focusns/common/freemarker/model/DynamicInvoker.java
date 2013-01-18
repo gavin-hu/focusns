ffbfcc2b48294bfb690710a79f0475fd05e2ca7b
Gavin Hu <haozhonghu@hotmail.com>
2013-01-19 00:40:06 +0800

    Add dynamic bean invoker for freemarker template

---
+++ focusns-base/focusns-runtime/src/main/java/org/focusns/common/freemarker/model/DynamicInvoker.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2011-2013 FocusSNS.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+package org.focusns.common.freemarker.model;
+
+import freemarker.ext.beans.BeanModel;
+import freemarker.ext.servlet.FreemarkerServlet;
+import freemarker.template.SimpleHash;
+import freemarker.template.TemplateModelException;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.util.Assert;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DynamicInvoker {
+
+    private SimpleHash simpleHash;
+
+    private Map<String, BeanWrapper> beanWrapperMap = new HashMap<String, BeanWrapper>();
+
+    public DynamicInvoker(SimpleHash simpleHash) {
+        this.simpleHash = simpleHash;
+    }
+
+    public void create(String name) {
+        String key = generateKey(name);
+        Object target = findTarget(name);
+        // target is null
+        if(target==null) {
+            return ;
+        }
+        // check type
+        Assert.isAssignable(BeanModel.class, target.getClass());
+        target = ((BeanModel)target).getWrappedObject();
+        // new BeanWrapper
+        BeanWrapper beanWrapper = new BeanWrapperImpl(target);
+        beanWrapper.setAutoGrowNestedPaths(true);
+        beanWrapperMap.put(key, beanWrapper);
+    }
+
+    public Object invoke(String path) {
+        String name = path.substring(0, path.indexOf("."));
+        String propertyPath = path.substring(path.indexOf(".") + 1);
+        String key = generateKey(name);
+        BeanWrapper beanWrapper = beanWrapperMap.get(key);
+        // check beanWrapper
+        if(beanWrapper==null) {
+            return null;
+        }
+        //
+        return beanWrapper.getPropertyValue(propertyPath);
+    }
+
+    private String generateKey(String name) {
+        return name.concat(Thread.currentThread().getName());
+    }
+
+    private Object findTarget(String name) {
+        Object target = null;
+        try {
+            SimpleHash model = (SimpleHash) simpleHash.get(FreemarkerServlet.KEY_REQUEST);
+            target = model.get(name);
+            //
+            if(target==null) {
+                model = (SimpleHash) simpleHash.get(FreemarkerServlet.KEY_SESSION);
+                target = model.get(name);
+            }
+            //
+            if(target==null) {
+                model = (SimpleHash) simpleHash.get(FreemarkerServlet.KEY_APPLICATION);
+                target = model.get(name);
+            }
+        } catch (TemplateModelException e) {
+            e.printStackTrace();
+        }
+        //
+        return target;
+    }
+
+}
--- focusns-base/focusns-runtime/src/main/java/org/focusns/web/widget/engine/impl/DefaultWidgetView.java
+++ focusns-base/focusns-runtime/src/main/java/org/focusns/web/widget/engine/impl/DefaultWidgetView.java
@@ -21,6 +21,7 @@ package org.focusns.web.widget.engine.impl;
 import freemarker.ext.servlet.FreemarkerServlet;
 import freemarker.template.SimpleHash;
 import freemarker.template.Template;
+import org.focusns.common.freemarker.model.DynamicInvoker;
 import org.focusns.web.widget.WidgetRequest;
 import org.focusns.web.widget.WidgetResponse;
 import org.focusns.web.widget.engine.WidgetView;
@@ -47,8 +48,9 @@ public class DefaultWidgetView implements WidgetView {
 		simpleHash.put(FreemarkerServlet.KEY_REQUEST, request.getRequestAttributes());
 		simpleHash.put(FreemarkerServlet.KEY_SESSION, request.getSessionAttributes());
 		simpleHash.put(FreemarkerServlet.KEY_APPLICATION, request.getApplicationAttributes());
+        simpleHash.put(DynamicInvoker.class.getSimpleName(), new DynamicInvoker(simpleHash));
 		//
 		return simpleHash;
-	}
+    }
 
 }
