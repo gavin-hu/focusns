3b33e60abbe069df463e0e31bb43c32f0d03c0a8
Gavin Hu <haozhonghu@hotmail.com>
2013-01-31 17:41:14 +0800

    File header updated by license-maven-plugin

--- focusns-base/focusns-runtime/src/main/java/org/focusns/social/api/SinaApi.java
+++ focusns-base/focusns-runtime/src/main/java/org/focusns/social/api/SinaApi.java
@@ -2,12 +2,32 @@
 
 package org.focusns.social.api;
 
-import org.scribe.builder.api.DefaultApi10a;
+/*
+ * #%L
+ * FocusSNS Runtime
+ * %%
+ * Copyright (C) 2011 - 2013 FocusSNS
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+
 import org.scribe.builder.api.DefaultApi20;
 import org.scribe.model.OAuthConfig;
-import org.scribe.model.Token;
 import org.scribe.model.Verb;
-import org.scribe.oauth.OAuthService;
 
 public class SinaApi extends DefaultApi20 {
 
