3608cfb0ede2dfdef1aa2b69fda6867012f6a0a6
haozhonghu@hotmail.com <520@xiaojin>
2013-07-06 19:10:19 +0800

    删除 KindEditor，改用百度的 UEditor...

---
+++ focusns-web/src/main/webapp/WEB-INF/assets/ueditor/ueditor.config.toolbar.js
@@ -0,0 +1,9 @@
+var editor_simple = {
+    initialFrameHeight : 250,
+    toolbars : [
+        ['fullscreen', 'source', '|', 'undo', 'redo', '|',
+            'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
+            'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
+            'help']
+    ]
+};
