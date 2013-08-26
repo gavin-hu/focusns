3eeea12403c10e41e82c4d8af6abdc132a4c6772
haozhonghu@hotmail.com <haozhonghu@hotmail.com>
2013-08-26 21:44:56 +0800

    MessageService 方法名重构

--- focusns-base/focusns-runtime/src/main/java/org/focusns/dao/msg/impl/MessageDaoImpl.java
+++ focusns-base/focusns-runtime/src/main/java/org/focusns/dao/msg/impl/MessageDaoImpl.java
@@ -7,16 +7,16 @@ package org.focusns.dao.msg.impl;
  * Copyright (C) 2011 - 2013 FocusSNS
  * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 2.1 of the 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
+ *
+ * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
@@ -34,18 +34,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class MessageDaoImpl extends MyBatisDaoSupport<Message> implements MessageDao {
 
-    public Page<Message> selectByProjectId(Page<Message> page, long projectId) {
+    public Page<Message> fetchByProjectId(Page<Message> page, long projectId) {
         //
         Map parameter = createParameter(projectId, null);
         //
-        return fetchPage("selectByProjectId", page, parameter);
+        return fetchPage("fetchByProjectId", page, parameter);
     }
 
-    public Page<Message> selectByToProjectId(Page<Message> page, long toProjectId) {
+    public Page<Message> fetchByToProjectId(Page<Message> page, long toProjectId) {
         //
         Map parameter = createParameter(null, toProjectId);
         //
-        return fetchPage("selectByToProjectId", page, parameter);
+        return fetchPage("fetchByToProjectId", page, parameter);
     }
 
     private Map createParameter(Long projectId, Long toProjectId) {
--- focusns-base/focusns-runtime/src/main/java/org/focusns/service/msg/impl/MessageServiceImpl.java
+++ focusns-base/focusns-runtime/src/main/java/org/focusns/service/msg/impl/MessageServiceImpl.java
@@ -7,16 +7,16 @@ package org.focusns.service.msg.impl;
  * Copyright (C) 2011 - 2013 FocusSNS
  * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 2.1 of the 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
+ *
+ * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
@@ -56,10 +56,10 @@ public class MessageServiceImpl implements MessageService {
     public Page<Message> fetchPageByBox(Page<Message> page, String box, long projectId) {
         //
         if ("IN".equalsIgnoreCase(box)) {
-            page = messageDao.selectByToProjectId(page, projectId);
+            page = messageDao.fetchByToProjectId(page, projectId);
         }
         if ("SENT".equalsIgnoreCase(box)) {
-            page = messageDao.selectByProjectId(page, projectId);
+            page = messageDao.fetchByProjectId(page, projectId);
         }
         //
         return page;
