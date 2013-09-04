4c92f0d6a8a0e8830e233f5fb0eb6cf9138c7002
haozhonghu@hotmail.com <haozhonghu@hotmail.com>
2013-09-04 17:01:31 +0800

    [代码重构] 删除无用权限控制相关代码

--- focusns-web/src/main/java/org/focusns/web/console/ConsoleController.java
+++ focusns-web/src/main/java/org/focusns/web/console/ConsoleController.java
@@ -7,30 +7,27 @@ package org.focusns.web.console;
  * Copyright (C) 2011 - 2013 FocusSNS
  * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 2.1 of the 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
+ *
+ * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
  */
 
-import java.util.Properties;
-
-import org.focusns.model.console.User;
-import org.focusns.service.auth.AuthenticationException;
-import org.focusns.web.helper.PropertyHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.Properties;
 
 @Controller
 @RequestMapping("/console")
@@ -38,18 +35,18 @@ public class ConsoleController {
 
     @Autowired
     private Properties application;
-
-    @RequestMapping("/login")
-    public void login(User user, WebRequest webRequest) {
-        String username = PropertyHelper.getConsoleUsername(application);
-        String password = PropertyHelper.getConsolePassword(application);
-        if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-            //
-            webRequest.setAttribute("user", user, WebRequest.SCOPE_SESSION);
-        } else {
-            throw new AuthenticationException("Username or Password miss matched!");
-        }
-    }
+//
+//    @RequestMapping("/login")
+//    public void login(User user, WebRequest webRequest) {
+//        String username = PropertyHelper.getConsoleUsername(application);
+//        String password = PropertyHelper.getConsolePassword(application);
+//        if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+//            //
+//            webRequest.setAttribute("user", user, WebRequest.SCOPE_SESSION);
+//        } else {
+//            throw new AuthenticationException("Username or Password miss matched!");
+//        }
+//    }
 
     @RequestMapping("/logout")
     public String logout(WebRequest webRequest) {
